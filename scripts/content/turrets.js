9549eac8fb4d8f920199bdb314ba55fdc81d0468
Joshwelyt <71865382+Joshwelyt@users.noreply.github.com>
2021-02-11 21:50:44 +0800

    Add files via upload

---
+++ scripts/content/turrets.js
@@ -0,0 +1,25 @@
+Vars.content.blocks().each(e => {
+    if(e instanceof Turret) {
+        e.reloadTime = 50;
+        e.spread = 0;
+        e.inaccuracy = 45;
+        e.recoilAmount = 2;
+        e.restitution = 0;
+        e.xRand = 0;
+        e.cooldown = 100;
+        e.rotateSpeed = 3;
+        e.targetGround = true;
+        e.targetAir = true;
+        if(typeof(e.shootType) !== "undefined") {
+            if(typeof(e.shootType.collidesGround) !== "undefined") e.shootType.collidesGround = true;
+            if(typeof(e.shootType.collidesAir) !== "undefined") e.shootType.collidesAir = true;
+        }
+    }
+});
+
+Blocks.lancer.chargeTime = 90;
+Blocks.lancer.chargeMaxDelay = 90;
+
+Blocks.meltdown.range = 70;
+Blocks.meltdown.shootDuration = 70;
+Blocks.meltdown.shootType.length = 50;
